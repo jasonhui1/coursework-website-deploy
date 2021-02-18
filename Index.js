a94827ad78b3306e957297f06361bdccbc7bf7bc
Jason <jason903813@gmail.com>
2021-02-18 19:22:02 +0000

    Update Index.js

--- Index.js
+++ Index.js
@@ -8,7 +8,7 @@ const app = express();
 
 //change port
 const port = process.env.PORT || 3000
-app.listen(3000, () => console.log('listening at 3000'));
+app.listen(port, () => console.log(`listening at ${port}`));
 
 //folder name
 app.use(express.static('public'));
